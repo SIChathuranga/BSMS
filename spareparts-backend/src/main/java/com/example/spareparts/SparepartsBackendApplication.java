cd7400ecce410bd14bd67b8fc0f893f4d869ad89
S.I.Chathuranga <134764717+SIChathuranga@users.noreply.github.com>
2025-08-12 15:41:36 +0530

    Initial commit

---
+++ spareparts-backend/src/main/java/com/example/spareparts/SparepartsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.spareparts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparepartsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparepartsBackendApplication.class, args);
+	}
+
+}
